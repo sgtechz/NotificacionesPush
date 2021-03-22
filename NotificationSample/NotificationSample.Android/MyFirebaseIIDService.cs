e915b9031458ffdb59faae01d18b1d4e179d311e
Leandro Samuel Gonzalez <lsgonzalez@baufest.com>
2021-03-22 20:39:08 -0300

    initial commit

---
+++ NotificationSample/NotificationSample.Android/MyFirebaseIIDService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+using Firebase.Messaging;
+
+namespace NotificationSample.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class MyFirebaseIIDService : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            SendRegistrationToServer(refreshedToken);
+        }
+
+        void SendRegistrationToServer(string token)
+        {
+            // Mandamos el token al server 
+        }
+
+        public void sedRegisterToken(string token)
+        {
+            //Tu código para registrar el token a tu servidor y base de datos
+        }
+    }
+}
---
+++ NotificationSample/NotificationSample.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace NotificationSample.Droid
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        public MyFirebaseMessagingService()
+        {
+
+        }
+
+        //public void OnNewToken(string token)
+        //{
+        //    base.OnNewToken(token);
+
+        //    Preferences.Set("TokenFirebase", token);
+        //    sedRegisterToken(token);
+        //}
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+            new NotificationHelper().CreateNotification(message.GetNotification().Title, message.GetNotification().Body);
+        }
+
+        //public void sedRegisterToken(string token)
+        //{
+        //    //Tu código para registrar el token a tu servidor y base de datos
+        //}
+    }
+}
